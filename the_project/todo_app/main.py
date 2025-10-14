2c7867bfa46cc4eb8bdab0ab851cd1399cef5560
Ilkka Kujamaki <ikujamaki@gmail.com>
2025-10-14 19:37:26 +0300

    Fix logging to work with gunicorn

--- the_project/todo_app/main.py
+++ the_project/todo_app/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import os
+import logging
 
 class TodoApp:
-    def __init__(self):
+    def __init__(self):        
         self.port = int(os.environ.get('PORT', 5000))
         self.flask_app = Flask(__name__)
         self.setup_routes()
@@ -13,7 +14,7 @@ class TodoApp:
             return f"Server started in port {self.port}"
 
     def run(self):
-        print(f"Server started in port {self.port}")
+        self.flask_app.logger.info(f"Server started in port {self.port}")
         self.flask_app.run(host='0.0.0.0', port=self.port)
 
 todo_app = TodoApp()
@@ -21,3 +22,8 @@ flask_app = TodoApp().flask_app
 
 if __name__ == '__main__':
     todo_app.run()
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    flask_app.logger.handlers = gunicorn_logger.handlers
+    flask_app.logger.setLevel(gunicorn_logger.level)
+    flask_app.logger.info(f"Server started in port {todo_app.port}")
