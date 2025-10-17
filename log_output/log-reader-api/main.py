0c3194c28433e8bd5964b61df448755ced32d13e
Ilkka Kujamaki <ikujamaki@gmail.com>
2025-10-17 07:17:21 +0300

    Read pong count from REST API instead of file

--- log_output/log-reader-api/main.py
+++ log_output/log-reader-api/main.py
@@ -1,30 +1,30 @@
 import os
 import logging
 from flask import Flask
+import requests
 
 LOG_FILE_PATH = os.environ.get('LOG_FILE_PATH', 
                                "/tmp/log_output/random.log")
 
-PONG_COUNT_FILE_PATH = os.environ.get('PONG_COUNT_FILE_PATH', 
-                                    '/tmp/pong_log/pong_count')
+PONG_COUNT_SERVICE_URL = os.environ.get('PONG_COUNT_SERVICE_URL',
+                                      "http://127.0.0.1:1122/pings")
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
 def get_log_string():
+    # Read the log file
     try:
         with open(LOG_FILE_PATH, "r") as f:
             log = f.read()
     except Exception as e:
         return f"Error reading log file: {e}"
     
-    try:
-        with open(PONG_COUNT_FILE_PATH, "r") as f:
-            pong_count = f.read()
-    except Exception as e:
-        return f"Error reading pong count file: {e}"
-    
-    resp = log + f"Ping / Pongs: {pong_count}"
+    # Call the /pings endpoint of the pong-count service
+    pings_resp = requests.get(PONG_COUNT_SERVICE_URL).json()
+    app.logger.debug("/pings response: %s", pings_resp)
+
+    resp = log + f"Ping / Pongs: {pings_resp['pong_count']}"
     
     return resp
 
@@ -35,6 +35,6 @@ def log_endpoint():
 if __name__ == "__main__":
     app.logger.info("Starting log reader API")
     app.logger.info("Log file path: %s", LOG_FILE_PATH)
-    app.logger.info("Pong count file path: %s", PONG_COUNT_FILE_PATH)
+    app.logger.info("Pong count API URL: %s", PONG_COUNT_SERVICE_URL)
 
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
