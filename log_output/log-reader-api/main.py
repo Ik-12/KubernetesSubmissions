6a980022468c8fe2b7ef1896396bf9a94f456501
Ilkka Kujamäki <ikujamaki@gmail.com>
2025-11-23 11:24:32 +0200

    Autoformatting changes

--- log_output/log-reader-api/main.py
+++ log_output/log-reader-api/main.py
@@ -1,19 +1,20 @@
-import os
 import logging
-from flask import Flask
+import os
+
 import requests
+from flask import Flask
 
-LOG_FILE_PATH = os.environ.get('LOG_FILE_PATH', 
-                               "/tmp/log_output/random.log")
+LOG_FILE_PATH = os.environ.get("LOG_FILE_PATH", "/tmp/log_output/random.log")
 
-INFO_FILE_PATH = os.environ.get('INFO_FILE_PATH', 
-                               "/tmp/information.txt")
+INFO_FILE_PATH = os.environ.get("INFO_FILE_PATH", "/tmp/information.txt")
 
-PONG_COUNT_SERVICE_URL = os.environ.get('PONG_COUNT_SERVICE_URL',
-                                      "http://127.0.0.1:1122/pings")
+PONG_COUNT_SERVICE_URL = os.environ.get(
+    "PONG_COUNT_SERVICE_URL", "http://127.0.0.1:1122/pings"
+)
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
+
 
 def get_log_string():
     # Read the log file
@@ -22,28 +23,37 @@ def get_log_string():
             log = f.read()
     except Exception as e:
         return f"Error reading log file: {e}"
-    
+
     # Call the /pings endpoint of the pong-count service
     try:
         response = requests.get(PONG_COUNT_SERVICE_URL, timeout=3)
         response.raise_for_status()
         pings_resp = response.json()
-        app.logger.debug("/pings response: %s", pings_resp if 'pings_resp' in locals() else "No response")
+        app.logger.debug(
+            "/pings response: %s",
+            pings_resp if "pings_resp" in locals() else "No response",
+        )
 
-        pong_count = pings_resp.get('pong_count', 'N/A')
+        pong_count = pings_resp.get("pong_count", "N/A")
     except Exception as e:
         app.logger.error("Error calling pong-count service: %s", e)
         pong_count = "Error getting pong count"
-    
+
     resp = log + f"Ping / Pongs: {pong_count}"
-    
+
     # Include the MESSAGE environment variable and file content from
     # config map
-    resp = f"env variable: MESSAGE={os.getenv("MESSAGE")}" + "\n" \
-        + "file content:" + open(INFO_FILE_PATH, "r").read() + "\n" \
+    resp = (
+        f"env variable: MESSAGE={os.getenv('MESSAGE')}"
+        + "\n"
+        + "file content:"
+        + open(INFO_FILE_PATH, "r").read()
+        + "\n"
         + resp
-    
+    )
+
     return resp
+
 
 @app.route("/log")
 def log_endpoint():
@@ -60,4 +70,4 @@ if __name__ == "__main__":
     app.logger.info("Log file path: %s", LOG_FILE_PATH)
     app.logger.info("Pong count API URL: %s", PONG_COUNT_SERVICE_URL)
 
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- ping_pong/main.py
+++ ping_pong/main.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
 import os
-import logging
-import psycopg2
 
-POSTGRES_URL = os.environ.get('POSTGRES_URL', 'postgresql://postgres@localhost')
+import psycopg2
+from flask import Flask, jsonify
+
+POSTGRES_URL = os.environ.get("POSTGRES_URL", "postgresql://postgres@localhost")
+
 
 class PingPongApp:
     def __init__(self):
-        self.port = int(os.environ.get('PORT', 5001))
+        self.port = int(os.environ.get("PORT", 5001))
 
         self.conn = self.init_db()
         self.flask_app = Flask(__name__)
@@ -37,18 +38,20 @@ class PingPongApp:
 
     def increment_pong_count(self):
         with self.conn.cursor() as cur:
-            cur.execute("UPDATE pong_counter SET pong_count = pong_count + 1 WHERE id=1;")
+            cur.execute(
+                "UPDATE pong_counter SET pong_count = pong_count + 1 WHERE id=1;"
+            )
             self.conn.commit()
 
     def setup_routes(self):
-        @self.flask_app.route('/pingpong')
+        @self.flask_app.route("/pingpong")
         def pong():
             count = self.get_pong_count()
             resp = f"pong {count}"
             self.increment_pong_count()
             return resp
 
-        @self.flask_app.route('/pings')
+        @self.flask_app.route("/pings")
         def pings():
             count = self.get_pong_count()
             return jsonify({"pong_count": count})
@@ -60,11 +63,11 @@ class PingPongApp:
     def run(self):
         self.flask_app.logger.info(f"Server started in port {self.port}")
         self.flask_app.logger.info(f"Using Postgres at: {POSTGRES_URL}")
-        self.flask_app.run(host='0.0.0.0', port=self.port)
+        self.flask_app.run(host="0.0.0.0", port=self.port)
+
 
 ping_pong_app = PingPongApp()
 flask_app = ping_pong_app.flask_app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ping_pong_app.run()
-
