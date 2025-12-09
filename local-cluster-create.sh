329d2eff8380046864b15547c40b1ee784fe297a
Ilkka Kujamäki <ikujamaki@gmail.com>
2025-12-09 10:48:23 +0200

    Configure staging and production env for todo app

--- local-cluster-create.sh
+++ local-cluster-create.sh
@@ -12,15 +12,23 @@ kubectl apply -f volumes/
 
 # Create secrets
 export SOPS_AGE_KEY_FILE=$HOME/key.txt
-sops --decrypt the_project/manifest/ssecret.enc.yaml | kubectl apply -f -
 
 # Create GCP Artifact Registry docker-registry secret in all namespaces
 namespaces=$(kubectl get ns -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 for ns in $namespaces; do
+    # Skip kube-system and default namespaces
+    if [[ "$ns" == "kube-"* || "$ns" == "default" || "$ns" == "argocd" ]]; then
+        continue
+    fi
+    
+    echo "Creating secrets in namespace: $ns"
+    kubectl apply -f ~/.secrets/broadcaster.yaml -n $ns
+    sops --decrypt the_project/manifests/secret.enc.yaml | grep -v "namespace: " | kubectl -n "$ns" apply -f - 
+    
     kubectl create secret docker-registry gcp-artifact-registry \
        --docker-server=europe-north1-docker.pkg.dev \
        --docker-username=_json_key \
        --docker-password="$(cat ~/.secrets/sa-docker-pull-from-local.key)" \
-       --docker-email=ikujamaki@gmail.com
+       --docker-email=ikujamaki@gmail.com -n $ns
 done
 
